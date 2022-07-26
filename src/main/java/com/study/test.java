de847c7e0770df66cbfcc6f9deb101e19f7c9301
daiweihao <8295850@qq.com>
2022-07-26 16:45:52 +0800

    代威浩合并了分支

--- src/main/java/com/study/test.java
+++ src/main/java/com/study/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello,world!");
         System.out.println("代威浩修改了40");
+        System.out.println("代威浩修改了45");
         System.out.println("代威浩修改了47");
     }
 }
