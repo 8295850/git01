30badb832261d6c86c4049fb690f407b2122338a
daiweihao <8295850@qq.com>
2022-07-26 16:33:46 +0800

    代威浩更改了main方法

--- src/main/java/com/study/test.java
+++ src/main/java/com/study/test.java
@@ -3,5 +3,8 @@ package com.study;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello,world!");
+        System.out.println("hello,world!");
+        System.out.println("hahahah");
+
     }
 }
