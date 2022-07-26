00bedd2f205eb1fffea7ca8999e14045464e8e21
daiweihao <8295850@qq.com>
2022-07-26 16:48:00 +0800

    代威浩更改了main方法46

--- src/main/java/com/study/test.java
+++ src/main/java/com/study/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello,world!");
         System.out.println("代威浩修改了40");
+        System.out.println("代威浩修改了47");
     }
 }
